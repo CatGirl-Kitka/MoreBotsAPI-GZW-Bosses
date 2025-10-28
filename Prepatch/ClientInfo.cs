4de486c3bece66c27148325fcaf7cb03ecd1cb86
Matthew Christensen <christensenfamily05@gmail.com>
2025-10-28 15:44:15 -0500

    Prepatch done

---
+++ Prepatch/ClientInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreBotsAPIExample
+{
+    public static class ClientInfo
+    {
+        public const string GUID = "com.morebotsexample.tacticaltoaster";
+        public const string PreLoadGUID = "com.morebotsexample.tacticaltoaster";
+        public const string PluginName = "MoreBotsAPI Example";
+        public const string PreLoadName = "MoreBotsPrepatch Example";
+        public const string Version = "1.0.0";
+    }
+}
---
+++ Prepatch/PrePatch.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using MoreBotsAPI;
+using System.Collections.Generic;
+
+namespace MoreBotsAPIExample
+{
+    [BepInDependency("com.morebotsapi.tacticaltoaster", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin(ClientInfo.PreLoadGUID, ClientInfo.PreLoadName, ClientInfo.Version)]
+    public class MoreBotsPrepatch : BaseUnityPlugin
+    {
+        public static MoreBotsPrepatch Instance;
+
+        public void Awake()
+        {
+            var exampleBot = new CustomWildSpawnType(1069, "bossExampleBot", "Boss", 1, true, false, false);
+
+            exampleBot.SetCountAsBossForStatistics(true);
+            exampleBot.SetShouldUseFenceNoBossAttack(true, false);
+            exampleBot.SetExcludedDifficulties(new List<int> { 0, 2, 3 }); // Exclude all difficulties except Normal. This is done by default if you do not set excluded difficulties.
+            
+            SAINSettings settings = new SAINSettings(exampleBot.WildSpawnTypeValue)
+            {
+                Name = "Example Bot",
+                Description = "An example bot created using MoreBotsAPI.",
+                Section = "Custom",
+                BaseBrain = "Assault",
+                BrainsToApply = new List<string> { "Assault" }, // Look for the ShortName in the class of the brains you want to apply to.
+            };
+
+            exampleBot.SetSAINSettings(settings);
+
+            CustomWildSpawnTypeManager.AddType(exampleBot);
+
+            CustomWildSpawnTypeManager.AddSuitableGroup(new List<int> { 1069, 1 }); // This allows the example bot to be the boss in a group with itself or normal scavs.
+        }
+    }
+}
