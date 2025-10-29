df67279c7a977b7a6bca2bf46e655c84b0fdab18
Matthew Christensen <christensenfamily05@gmail.com>
2025-10-28 23:54:58 -0500

    Better comments

--- Prepatch/Patches/CustomTypesPatch.cs
+++ Prepatch/Patches/CustomTypesPatch.cs
@@ -11,34 +11,37 @@ namespace MoreBotsAPIExample.Prepatch
     {
         public static IEnumerable<string> TargetDLLs { get; } = new[] { "Assembly-CSharp.dll" };
 
-        public static AssemblyDefinition patchAssembly;
-
         public static void Patch(ref AssemblyDefinition assembly)
         {
-            patchAssembly = assembly;
-
-            Logger.CreateLogSource("MoreBotsAPIExample").LogInfo("Creating bossExampleBot!");
-
+            // 1069 is the enum value for your custom type, followed by the enum name. The other variables are explained in the CustomWildSpawnType class comments
             var exampleBot = new CustomWildSpawnType(1069, "bossExampleBot", "Boss", 32, true, false, false);
 
             exampleBot.SetCountAsBossForStatistics(true);
+            // Should having max fence loyalty stop this bot from warning your pscav. Doesn't affect hostility (that is defined in the type json), only interaction with warn behavior.
             exampleBot.SetShouldUseFenceNoBossAttack(true, false);
-            exampleBot.SetExcludedDifficulties(new List<int> { 0, 2, 3 }); // Exclude all difficulties except Normal. This is done by default if you do not set excluded difficulties.
+            // Exclude all difficulties except Normal. This is done by default if you do not set excluded difficulties.
+            exampleBot.SetExcludedDifficulties(new List<int> { 0, 2, 3 });
 
             SAINSettings settings = new SAINSettings(exampleBot.WildSpawnTypeValue)
             {
                 Name = "Example Bot",
                 Description = "An example bot created using MoreBotsAPI.",
+                // This is the bot section your bot will appear under, like Bosses, Followers, PMCs, etc.
                 Section = "Custom",
+                // Not used unless you didn't define BrainsToApply
                 BaseBrain = "Assault",
-                BrainsToApply = new List<string> { "Assault" }, // Look for the ShortName in the class of the brains you want to apply to.
+                // Look for the ShortName in the class of the brains you want to apply to.
+                BrainsToApply = new List<string> { "Assault" }, 
             };
 
             exampleBot.SetSAINSettings(settings);
 
-            CustomWildSpawnTypeManager.RegisterWildSpawnType(exampleBot, assembly); // This is what registers your new spawn type, adding it to the WildSpawnType enum and .
+            // This is what registers your new spawn type, adding it to the WildSpawnType enum and manager for custom bot types.
+            CustomWildSpawnTypeManager.RegisterWildSpawnType(exampleBot, assembly);
 
-            CustomWildSpawnTypeManager.AddSuitableGroup(new List<int> { 1069, 1 }); // This allows the example bot to be the boss in a group with itself or normal scavs.
+            // This allows the example bot to be the boss in a group with itself or normal scavs.
+            // If you want your bot to be solo you don't have to worry about this.
+            CustomWildSpawnTypeManager.AddSuitableGroup(new List<int> { 1069, 1 });
         }
 
     }
