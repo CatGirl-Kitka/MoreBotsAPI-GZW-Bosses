3546ea34bb248cbf4cc98f61bf95e7dc2a76bf7f
Kitka <97989843+CatGirl-Kitka@users.noreply.github.com>
2026-09-03 05:50:40 +0200

    Add files via upload

---
+++ Plugin/WildSpawnTypeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using EFT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackDiv
+{
+    public static class WildSpawnTypeExtensions
+    {
+        public static List<int> BDTypeEnums = new List<int> { 848420, 848421, 848422, 848423, 848424, 848426 };
+
+        public static bool IsBlackDiv(WildSpawnType type)
+        {
+            return BDTypeEnums.Contains((int)type);
+        }
+    }
+}
